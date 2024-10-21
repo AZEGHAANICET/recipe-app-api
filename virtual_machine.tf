ac4ba2fc78b03be4df70deef257af03953314093
Anicet Azegha <loicanicetazegha@gmail.com>
2024-10-22 00:46:52 +0200

    Added Github

--- virtual_machine.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_virtual_network" "firstVM" {
-  name                = "first-virtual-machine"
-  location            = "RG-Abin-Patrick-Takam-Tayo"."firstVM"."Nord Europe"
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-
-
